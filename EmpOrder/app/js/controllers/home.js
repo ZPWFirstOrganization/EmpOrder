704809431636303182a982f4e1faa4ce084d347d
zzc <592836632@qq.com>
2016-01-21 18:18:01 +0800

    first commit

--- EmpOrder/app/js/controllers/home.js
+++
@@ -1,7 +0,0 @@
-var demoControllers = angular.module('demoControllers', []);
-
-// demoControllers.controller('homeController', ['$scope',function ($scope) {
-// 	$scope.pushPage = function (){
-// 		alert("push")
-// 	};
-// }]);
--- EmpOrder/app/js/controllers/page3.js
+++
@@ -1,7 +0,0 @@
-// var demoControllers = angular.module('demoControllers', []);
-demoControllers.controller('page3Controller', ['$scope',function ($scope) {
-	$scope.popPage = function(){
-		window.history.back(-1);
-		// window.location.href="login.jsp?backurl="+window.location.href
-	}
-}]);
