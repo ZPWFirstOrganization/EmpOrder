f4239bfd9e1b6113c237f486255d7a579a2bcbe8
hp-zpw <peng-wei.zhong@successinfo.com.cn>
2016-02-19 17:24:03 +0800

    fix some bug

--- orderApp/js/controllers/orderDetail.js
+++ orderApp/js/controllers/orderDetail.js
@@ -1,7 +1,7 @@
 orderApp.controller('orderDetailCtrl',function($scope,$state,$stateParams,ApiService,apiCaller,scopeData){
 	$scope.orderData
-	apiCaller.getOrderDetailInfo({orderID:"7f845abe-d177-4aaa-bf74-05f94bab0603"},function(res){
-		// console.log(res[0])
+	apiCaller.getOrderDetailInfo({orderID:$stateParams.orderID},function(res){
+		console.log(res[0])
 		$scope.orderData = res[0]
 	},function(res){
 		$scope.orderData = {product:{}}
